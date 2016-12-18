7b3b7d114827fb86901a3c2cfe67d2954ef20546
yvonne johnson webb <ymjwcoder16@gmail.com>
2016-12-17 22:35:46 -0800

    first commit

---
+++ moduleMammal.rb
@@ -0,0 +1,19 @@
+module Mammal
+    class Dog
+        def speak
+            puts "Woof!"
+        end
+    end
+
+    class Cat
+        def speak
+            puts "Meow"
+        end
+    end
+end
+
+a = Mammal::Dog.new
+b = Mammal::Cat.new
+
+a.speak
+b.speak
